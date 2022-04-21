f2a9572eacde0887c0f124a04b5c415ee5d40325
hari.praghash <hari.praghash@pollinate.co.uk>
2022-04-21 10:25:16 +0530

    fix

--- import-piper-state.sh
+++ import-piper-state.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-piper_states=("module.piper_aks.azuread_application.aks" "module.piper_aks.azuread_service_principal.aks" 
+piper_aad_resources=("module.piper_aks.azuread_application.aks" "module.piper_aks.azuread_service_principal.aks" 
 "module.piper_aks.azuread_application_password.aks" "module.piper_aks.azuread_service_principal_password.aks")
 
-for state in ${piper_states[@]}; do
-  echo "Importing state for $state"
-  resource=$(terraform state list | grep $state)
-  if [ -z "$resource" ]; then
-    echo "No state found for $state therefore will import"
-    terraform import 
+for resource in ${piper_aad_resources[@]}; do
+  echo "Importing state for $resource"
+  existingresource=$(terraform state list | grep $resource)
+  if [ -z "$existingresource" ]; then
+    echo "No state found for $resource therefore will import"
+    resource_id=$(terraform show -json "${OLD_STATE_PATH}/${OLD_STATE_FILE_NAME}" | jq --compact-output --raw-output '.values.root_module.child_modules[].resources[] | select (.address == "${resource}").values.id') 
+    terraform import "${resource}" "${resource_id}"
   else
     echo "State found for $state"
   fi
